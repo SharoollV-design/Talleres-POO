ebb083704d8e82aa0f692a2ef1d6034ec38659b4
Sharooll <smezav1@unicartagena.edu.co>
2026-04-05 18:37:50 -0500

    Se agrego el metodo vender(int cantidad) a la clase Producto

--- Taller_0/Producto.java
+++ Taller_0/Producto.java
@@ -9,4 +9,12 @@ public class Producto {
         System.out.println("Precio: " + precio);
         System.out.println("Stock: " + stock);
     }
+
+    void vender(int cantidad) {
+        if(stock >= cantidad) {
+            stock -= cantidad;
+        } else {
+            System.out.println("No tiene el suficiente stock");
+        }
+    }
 }
