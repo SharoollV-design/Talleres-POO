91802c30bde117d9239e36ba8e72d0347eb5974b
Sharooll <smezav1@unicartagena.edu.co>
2026-04-05 12:21:29 -0500

    Bloque B. Construccion de objetos, iniciamos con el ejercicio 6

--- Estudiante.java
+++ Estudiante.java
@@ -1,4 +1,4 @@
-class Estudiante{
+public class Estudiante{
     String nombre; 
     String codigo;
     int semestre;
@@ -7,6 +7,17 @@ class Estudiante{
         System.out.println("Nombre: " + nombre);
         System.out.println("Codigo estudiantil: " + codigo);
         System.out.println("Semestre: " + semestre);
+    }
+}
+
+class main {
+    public static void main(String[] args) {
+        Estudiante est1 = new Estudiante();
+        est1.nombre = "Jessica Mateus";
+        est1.codigo = "est1.Jess2026";
+        est1.semestre = 6;
+
+        est1.mostrarInfo();
     }
 
 }
