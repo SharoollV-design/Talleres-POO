0ad72cfb6a8c0f7d06df1bbc4298f7e91771c49c
Sharooll <smezav1@unicartagena.edu.co>
2026-04-06 15:44:28 -0500

    Ejercicio 48: Confundir atributos con variables locales

--- Taller_0/Mascota.java
+++ Taller_0/Mascota.java
@@ -3,7 +3,14 @@ public class Mascota {
     String nombre;
     String especie;
     int edad;
+    
+    // Ejercicio 48: Confundir atributos 
+    // void mostrarMascota() {
+    // String nombre = "Algodon";
+    // System.out.println(nombre);
+    //}
 
+    // Forma correcta:
     void mostrarMascota() {
         System.out.println("Nombre de la mascota: " + nombre);
         System.out.println("Especie: " + especie);
