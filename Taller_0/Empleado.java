e37223443644d686f9d8d54920487efc3a0229aa
Sharooll <smezav1@unicartagena.edu.co>
2026-04-06 13:23:57 -0500

    Ejercicio 41: Crear clase Empleado

---
+++ Taller_0/Empleado.java
@@ -0,0 +1,13 @@
+package Taller_0;
+
+public class Empleado {
+    String nombre;
+    String puesto;
+
+    public Empleado(String nombre, String puesto) {
+        this.nombre = nombre;
+        this.puesto = puesto;
+    }
+}
+
+//Empleado empleado1 = new Empleado("Carlos R", "Mensajero");
