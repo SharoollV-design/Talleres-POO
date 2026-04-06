26fcb99b249d4d6701471551ee7c1fbca509753b
Sharooll <smezav1@unicartagena.edu.co>
2026-04-06 11:08:31 -0500

    Ejercicio 37: Crear una clase celular con sus metodos y constructor

--- Taller_0/Celular.java
+++ Taller_0/Celular.java
@@ -1,5 +1,32 @@
 package Taller_0;
 public class Celular {
-    
-    
+    String marca; 
+    int bateria;
+    boolean prendido;
+
+    public Celular(String marca, int bateria) {
+        this.marca = marca;
+        this.bateria = bateria;
+        this.prendido = false;
+    }
+
+    void encender() {
+        prendido = true;
+    }
+
+    void apagar() {
+        prendido = false;
+    }
+
+    void cargarBateria() {
+        if (bateria <= 20) {
+            System.out.println("Bateria baja! Cargue el celular");
+        }
+    }
+
+    void mostrarInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Bateria: " + bateria);
+         System.out.println("Celular " + (prendido ? "Encendido" : "Apagado"));
+    }
 }
