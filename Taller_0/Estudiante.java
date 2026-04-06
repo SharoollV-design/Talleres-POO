0ca8cc1f565b59a6f39149adf3a47c19c613e59d
Sharooll <smezav1@unicartagena.edu.co>
2026-04-05 19:45:24 -0500

    Ejercicio 19, se agregaron los comentarios entre las diferencias de clase vs objeto

--- Taller_0/Estudiante.java
+++ Taller_0/Estudiante.java
@@ -13,4 +13,10 @@ public class Estudiante{
     void cambiarSemestre(int nuevoSemestre) {
         semestre = nuevoSemestre;
     }
+
+    // Estudiante es una clase ya que es el molde básico en el que se define un nuevo dato
+    // además describe que caracteristicas tendrá algo. 
+
+    // Por otro lado est1 es un objeto ya que es una entidad creada a partir e la clase Estudiante
+    // Tiene identidad propia y la posibilidad de ejecutar comportamientos, como su nombre. 
 }
