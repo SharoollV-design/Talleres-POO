10131091d9a9c1bd4f3ad68435823c8bf1aa6d55
Sharooll <smezav1@unicartagena.edu.co>
2026-04-06 16:35:55 -0500

    Ejercicio 51: Crear una clase rectagunlo con los metodos calcular area y perimetro

---
+++ Taller_0/Rectangulo.java
@@ -0,0 +1,30 @@
+package Taller_0;
+
+public class Rectangulo {
+    double altura;
+    double base;
+
+    public Rectangulo(double base, double altura) {
+        this.base= base;
+        this.altura = altura;
+    }
+
+    void calcularArea() {
+        double area = base * altura;
+        System.out.println("El area es: " + area);
+    }
+
+    void calcularPerimetro() {
+        double perimetro = 2 * (base+ altura);
+        System.out.println("El perimetro es: " + perimetro);
+    }
+
+    void mostrarResultado() {
+        System.out.println("Base: " + base);
+        System.out.println("Altura: " + altura);
+
+        calcularArea();
+        calcularPerimetro();
+    }
+    
+}
