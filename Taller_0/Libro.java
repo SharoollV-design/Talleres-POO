a36f82259193885d44c49481c1792e33ace66891
Sharooll <smezav1@unicartagena.edu.co>
2026-04-05 20:23:20 -0500

    Ejercicio 22: Se creo la clase Libro

---
+++ Taller_0/Libro.java
@@ -0,0 +1,13 @@
+package Taller_0;
+
+public class Libro {
+    String titulo;
+    String autor;
+    int cantidad;
+
+    void mostrarInfo() {
+        System.out.println("Titulo: " + titulo);
+        System.out.println("Autor del libro: " + autor);
+        System.out.println("Numero de paginas: " + cantidad);
+    }
+}
