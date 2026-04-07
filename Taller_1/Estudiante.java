8e51d21026f44c0dd50565e05474ce6237e3fd7a
Sharooll <smezav1@unicartagena.edu.co>
2026-04-07 18:41:46 -0500

    Ejercicio 3: Creamos una clase estudiante, un constructor por defecto, un constructor parametrizado y un constructor por parametros que permita llamar a otro por medio de this()

---
+++ Taller_1/Estudiante.java
@@ -0,0 +1,77 @@
+package Taller_1;
+
+public class Estudiante {
+    private String nombre;
+    private int edad;
+    private String curso;
+
+    public Estudiante() {
+        this.nombre = "Estudiante";
+        this.edad = 0;
+        this.curso = "No se registra curso";
+    }
+
+    public Estudiante(String nombre, int edad) {
+
+        if(nombre != null && !nombre.isEmpty()){
+            this.nombre = nombre;
+        } else {
+            System.out.println("No se registra nombre, se asigna por defecto 'Estudiante'");
+            this.nombre = "Estudiante";
+        }
+
+        if(edad>0){
+            this.edad = edad;
+        } else {
+            System.out.println("No se registra edad, se asigna por defecto '0'");
+            this.edad = 0;
+        }
+
+        this.curso = "No se registra curso";
+    }
+
+    public Estudiante(String nombre, int edad, String curso) {
+
+        this(nombre,edad);
+
+        if (curso != null && !curso.isEmpty()){
+            this.curso = curso;
+        }
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getEdad(){
+        return edad;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void SetNombre(String nombre){
+        if(nombre != null && !nombre.isEmpty()){
+            this.nombre = nombre;
+        } else {
+            System.out.println("No se registra nombre, se asigna por defecto 'Estudiante'");
+            this.nombre = "Estudiante";
+        }
+    }
+
+    public void SetEdad(int edad){
+        if(edad>0){
+            this.edad = edad;
+        } else {
+            System.out.println("No se registra edad, se asigna por defecto '0'");
+            this.edad = 0;
+        }
+    }
+
+    public void SetCurso(String curso){
+        if(curso != null && !curso.isEmpty()){
+            this.curso = curso;
+        }
+    }
+}
