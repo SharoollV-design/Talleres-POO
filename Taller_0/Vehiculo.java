353e4b459037b8395b8f94ea4da6819115c3f6b1
Sharooll <smezav1@unicartagena.edu.co>
2026-04-06 22:38:19 -0500

    Ejercicio 71: Reescribi el codigo agregandole un constructor con parametros

--- Taller_0/Vehiculo.java
+++ Taller_0/Vehiculo.java
@@ -4,6 +4,12 @@ public class Vehiculo {
     String modelo;
     int velocidadActual;
 
+    public Vehiculo(String marca, String modelo, int velocidadActual) {
+      this.marca = marca;
+      this.modelo = modelo;
+      this.velocidadActual = velocidadActual;
+    }
+
     void mostrarEstado() {
         System.out.println("Marca: " + marca);
         System.out.println("Modelo: " + modelo);
@@ -22,11 +28,11 @@ public class Vehiculo {
         }
    }
 }
-
-  //void frenar () {
+    // Ejercicio 45: caso hipotetico en el que el metodo quedara fuera de la clase
+    //void frenar () {
     //    if (velocidadActual >= 10) {
-     //       velocidadActual -= 10; 
+    //       velocidadActual -= 10; 
     //    } else {
     //        velocidadActual = 0;
-     //   }
-   // }
+    //   }
+    // }
