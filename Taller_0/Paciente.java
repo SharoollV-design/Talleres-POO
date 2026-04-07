e714654c15c6da609cbb70df2ae9af942971328a
Sharooll <smezav1@unicartagena.edu.co>
2026-04-06 22:49:12 -0500

    Ejercicio 73: Se agrego un constructor copia en la clase Paciente

--- Taller_0/Paciente.java
+++ Taller_0/Paciente.java
@@ -11,6 +11,12 @@ public class Paciente {
         this.edad = edad;
     }
 
+    public Paciente(Paciente otroPaciente) {
+        this.nombre = otroPaciente.nombre;
+        this.enfermedad = otroPaciente.enfermedad;
+        this.edad = otroPaciente.edad;
+    }
+
     void mostrarInfo() {
         System.out.println("Nombre: " + nombre);
         System.out.println("Enfermedad: " + enfermedad);
