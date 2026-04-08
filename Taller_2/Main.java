a0a7d8dfec23ae387e0831836df8b43d82d582e0
Sharooll <smezav1@unicartagena.edu.co>
2026-04-08 18:33:08 -0500

    Se probó la clase prueba ya corregida

--- Taller_2/Main.java
+++ Taller_2/Main.java
@@ -8,11 +8,19 @@ public class Main {
         Producto produc1 = new Producto("Camiseta", 50.000);
         produc1.mostrarProducto();
 
+        System.out.println("~~~~~");
         
         Estudiante est1 = new Estudiante();
         Estudiante est2 = new Estudiante("Sharoll", 20);
 
         est1.mostrarEstudiante();
         est2.mostrarEstudiante();
+
+        System.out.println("~~~~~");
+
+        //Ademas probamos la clase prueba
+
+        Prueba pr1 = new Prueba("Ana Mar"); 
+        pr1.mostrarNombre();
     }
 }
