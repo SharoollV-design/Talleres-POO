6a42b8f16bcf1b8892c1903ddfa4b4e5a5bafda4
Sharooll <smezav1@unicartagena.edu.co>
2026-04-08 17:37:02 -0500

    Ejercicio 1: Se agregaron metodos de set y get, además agregue validaciones para el precio

--- Taller_2/Producto.java
+++ Taller_2/Producto.java
@@ -9,6 +9,26 @@ public class Producto {
         this.precio = precio;
     }
 
+    public String getNombre() {
+        return nombre;
+    }
+
+    public double getPrecio(){
+        return precio;
+    }
+
+    public void SetNombre(String nombre){
+        this.nombre = nombre;
+    }
+
+    public void SetPrecio(double precio){
+        if (precio > 0){
+        this.precio = precio;
+        } else {
+            System.out.println("El precio no puede ser negativo");
+        }
+    }
+
     void mostrarProducto() {
         System.out.println("Bienvenido");
         System.out.println("Nombre del producto: " + nombre);
