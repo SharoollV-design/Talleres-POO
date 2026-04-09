d3165122fcceaae627c5c3fc9fcb6b2cb3e05a57
Sharooll <smezav1@unicartagena.edu.co>
2026-04-08 21:09:44 -0500

    Ejercicio 2: Se definio la clase matematicas con metodos estaticos que realiza operaciones basicas como la suma

--- taller_3/Matematicas.java
+++ taller_3/Matematicas.java
@@ -2,4 +2,24 @@ package taller_3;
 
 public class Matematicas {
     
+    public static double sumar(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public static double restar(double num1, double num2){
+        return num1 - num2;
+    }
+
+    public static double multiplicar(double num1, double num2){
+        return num1 * num2;
+    }
+
+    public static double dividir(double num1, double num2){
+        if (num2 == 0){
+            System.out.println("Numero errado: no se debe dividir en cero");
+            return 0;
+        }
+        return num1/num2;
+    }
+
 }
