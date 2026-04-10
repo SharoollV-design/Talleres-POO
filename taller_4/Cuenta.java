7701a0dffb25f928304215360c7f379363f03975
Sharooll <smezav1@unicartagena.edu.co>
2026-04-09 20:30:41 -0500

    Ejercicio final: Crear una clase con atributo privados sin utilizar get o set

--- taller_4/Cuenta.java
+++ taller_4/Cuenta.java
@@ -2,4 +2,11 @@ package taller_4;
 
 public class Cuenta {
     
+    private double saldo; // Atributo privado 
+
+    // No creamos ni metodo get ni metodo set
+
+    public Cuenta(double saldo){
+        this.saldo = saldo;
+    }
 }
