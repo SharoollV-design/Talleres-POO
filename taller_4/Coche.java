a5d26689c1f5fccfcade0e776c0c09f2abd94739
Sharooll <smezav1@unicartagena.edu.co>
2026-04-09 20:14:54 -0500

    Ejercicio 2: Ejercicio 2: cree una clase coche con un metodo que incrementa la velocidad, además de un metodo que muestre la informacion

--- taller_4/Coche.java
+++ taller_4/Coche.java
@@ -1,5 +1,71 @@
 package taller_4;
 
 public class Coche {
-    
+    private String marca;
+    private String modelo;
+    private double velocidad;
+
+    public Coche(){
+        this.marca = "Sin marca";
+        this.modelo = "Deconocido2";
+        this.velocidad = 0.0;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+
+    public String getModelo(){
+        return modelo;
+    }
+
+    public double getVelocidad(){
+        return velocidad;
+    }
+
+    public void SetMarca(String marca){
+        if(marca != null && !marca.isEmpty()) {
+            this.marca = marca;
+        } else {
+            System.out.println("No se registra marca");  
+        }
+    }
+
+    public void SetModelo(String modelo){
+        if(modelo != null && !marca.isEmpty()){
+            this.modelo = modelo;
+        } else {
+            System.out.println("Modelo desconocido");
+        }
+    }
+
+    public void SetVelocidad(double velocidad){
+        if (velocidad > 0) {
+            this.velocidad = velocidad;
+        } else {
+            System.out.println("La velocidad debe ser postiva");
+        }
+    }
+
+    public Coche(String marca, String modelo, double velocidad){
+        this.marca = marca;
+        this.modelo = modelo;
+        this.velocidad = velocidad;
+    }
+
+    public void acelerar(int incremento){
+        if (incremento > 0){
+            velocidad += incremento;
+            System.out.println("El coché acelero, nueva velocidad: " + velocidad);
+        } else {
+            System.out.println("El incremento debe ser positivo");
+        }
+    }
+
+    public void mostrarInfo(){
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Velocidad: " + velocidad);
+    }
+
 }
