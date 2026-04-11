3b4b58b253fcc05f370a70f9448b58ec080a20b6
Sharooll <smezav1@unicartagena.edu.co>
2026-04-11 16:37:09 -0500

    Taller 6: Cree una prueba de acceso desde otro package para ver los errores de compilacion a la hora de acceder a los datos protected

---
+++ PruebaAcceso.java
@@ -0,0 +1,13 @@
+import taller_6.Vehiculo;
+
+public class PruebaAcceso {
+    
+    public static void main(String[] args) {
+         
+        Vehiculo v1 = new Vehiculo("Terrestre", "Mazda");
+        
+        // Error 
+        //System.out.println(v1.tipo);
+        //System.out.println(v1.marca);
+    }
+}
