f11133b3ab7d0c6d3478917e25af1868356d0b46
Sharooll <smezav1@unicartagena.edu.co>
2026-04-11 16:17:26 -0500

    Ejercicio 2: Cree una clase vehiculo con modicadores de acceso protected

---
+++ taller_6/Vehiculo.java
@@ -0,0 +1,11 @@
+package taller_6;
+
+public class Vehiculo {
+    protected String tipo; 
+    protected String marca;
+
+    public Vehiculo(String tipo, String marca) {
+        this.tipo = tipo;
+        this.marca = marca;
+    }
+}
