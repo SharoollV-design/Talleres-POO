7ee10424a2f60b65fe12b130494f59f0d8ced096
Sharooll <smezav1@unicartagena.edu.co>
2026-04-11 16:35:14 -0500

    Ejercicio 2: Cree una clase de prueba para comprobar las clases pero dentro del package para ver su funcionamiento

---
+++ taller_6/PruebaVehiculos.java
@@ -0,0 +1,10 @@
+package taller_6;
+
+public class PruebaVehiculos {
+    
+    public static void main(String[] args) {
+        
+        Moto moto1 = new Moto("Terrestre", "Yamaha", 200);
+        moto1.mostrarDatos();
+    }
+}
